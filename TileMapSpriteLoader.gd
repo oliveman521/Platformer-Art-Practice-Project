4cb6160ee8533e9f58d602c3b15242f79d6ffa74
Oliver Philbrick <oliveman521@gmail.com>
2024-05-07 16:31:45 -0400

    Added PNG support + Added the default sprites

--- TileMapSpriteLoader.gd
+++ TileMapSpriteLoader.gd
@@ -9,11 +9,16 @@ func _ready() -> void:
 	
 	
 	var grass_image_file: Image = Image.load_from_file(folder_name + "/GrassTile.svg")
+	if(!grass_image_file): #also try png
+		grass_image_file = Image.load_from_file(folder_name + "/GrassTile.png")
+	
 	if(grass_image_file):
 		tile_set.get_source(0).texture =  ImageTexture.create_from_image(grass_image_file)
 	
 	
 	var dirt_image_file: Image = Image.load_from_file(folder_name + "/DirtTile.svg")
+	if(!dirt_image_file): #also try png
+		dirt_image_file = Image.load_from_file(folder_name + "/DirtTile.png")
 	if(dirt_image_file):
 		tile_set.get_source(1).texture =  ImageTexture.create_from_image(dirt_image_file)
 	
